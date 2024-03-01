5c96f56d0f1fa66fbb268459a7f59ac64b73a174
Vinod <vinodnyamagoudar@gmail.com>
2024-03-01 23:38:47 +0530

    Initial commit

--- app/src/main/java/com/example/greendzineapp/MainActivity.kt
+++ app/src/main/java/com/example/greendzineapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.greendzineapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.greendzineapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.btnLogin.setOnClickListener {
+            // validation of the email and password in the activity
+            startActivity(Intent(this@MainActivity,HomeActivity::class.java))
+            finish()
+        }
     }
 }
