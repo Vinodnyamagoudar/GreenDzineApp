6c681b73608b0e19171de8bd44e22c24b58d1a7b
Vinod <vinodnyamagoudar@gmail.com>
2024-03-01 22:53:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/greendzineapp/PersonFragment.kt
@@ -0,0 +1,18 @@
+package com.example.greendzineapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PersonFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_person, container, false)
+    }
+}
